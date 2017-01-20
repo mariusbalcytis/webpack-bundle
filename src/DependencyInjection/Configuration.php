6f61ccc59a09d2d327fda4b9e6f7ba1f85951ab3
Steven Pribilinskiy <steven.pribilinskiy@gmail.com>
2017-01-20 02:37:30 +0200

    :art: Convert configuration tree builder to hierarchy view

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,7 +2,6 @@
 
 namespace Maba\Bundle\WebpackBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -13,12 +12,22 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
  */
 class Configuration implements ConfigurationInterface
 {
-    protected $availableBundles;
     /**
-     * @var
+     * @var array
+     */
+    protected $availableBundles;
+
+    /**
+     * @var string
      */
     private $environment;
 
+    /**
+     * Configuration constructor.
+     *
+     * @param array $availableBundles
+     * @param string $environment
+     */
     public function __construct(array $availableBundles, $environment)
     {
         $this->availableBundles = $availableBundles;
@@ -35,120 +44,251 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('maba_webpack');
 
-        $children = $rootNode->children();
+        $rootNode->children() // @formatter:off
 
-        $assetProviders = $children->arrayNode('asset_providers');
-        $assetProviders->defaultValue(array(
-            array('type' => 'twig_bundles', 'resource' => $availableBundles),
-            array('type' => 'twig_directory', 'resource' => '%kernel.root_dir%/Resources/views')
-        ));
+            /**
+             * Asset paths
+             */
+            ->arrayNode('asset_providers')
+				->defaultValue([
+					[
+						'type' => 'twig_bundles',
+						'resource' => $availableBundles,
+					],
+					[
+						'type' => 'twig_directory',
+						'resource' => '%kernel.root_dir%/Resources/views',
+					],
+				])
+				->beforeNormalization()
+					->always(function ($value) use ($availableBundles) {
 
-        $assetProviders->beforeNormalization()->always(function($value) use ($availableBundles) {
-            foreach ($value as &$item) {
-                if ($item['type'] === 'twig_bundles' && empty($item['resource'])) {
-                    $item['resource'] = $availableBundles;
-                }
-            }
-            return $value;
-        });
+						foreach ($value as &$item) {
 
-        /** @var ArrayNodeDefinition $prototype  */
-        $prototype = $assetProviders->prototype('array');
-        $prototypeChildren = $prototype->children();
-        $prototypeChildren->scalarNode('type');
-        $prototypeChildren->variableNode('resource');
+							if ($item['type'] === 'twig_bundles' && empty($item['resource'])) {
+								$item['resource'] = $availableBundles;
+							}
+						}
 
-        $twig = $children->arrayNode('twig')->addDefaultsIfNotSet()->children();
-        $twig->scalarNode('function_name')
-            ->defaultNull()
-            ->info('Deprecated and will be removed in next version')
+						return $value;
+					})
+                ->end()
+				->prototype('array')
+					->children()
+						->scalarNode('type')->end()
+						->variableNode('resource')->end()
+					->end()
+                ->end()
+            ->end() // asset_providers
+
+            /**
+             * Twig error handling
+             */
+            ->arrayNode('twig')
+                ->addDefaultsIfNotSet()
+                ->children()
+
+                    ->scalarNode('function_name')
+                        ->defaultNull()
+						->info('Deprecated and will be removed in next version')
+                    ->end()
+
+					->scalarNode('suppress_errors')
+						->defaultValue(
+							$this->environment === 'dev' ? true : 'ignore_unknowns'
+						)
+						->validate()
+							->ifNotInArray([
+								true,
+								false,
+								'ignore_unknowns',
+							])
+							->thenInvalid('suppress_errors must be either a boolean or "ignore_unknowns"')
+                        ->end()
+					->end()
+
+                ->end()
+            ->end() // twig
+
+            /**
+             * Webpack config path and parameters
+             */
+            ->arrayNode('config')
+				->addDefaultsIfNotSet()
+				->children()
+
+                    ->scalarNode('path')
+				    	->defaultValue('%kernel.root_dir%/config/webpack.config.js')
+                    ->end()
+
+                    ->arrayNode('parameters')
+						->treatNullLike([])
+						->useAttributeAsKey('name')
+						->prototype('variable')->end()
+                    ->end()
+
+                ->end()
+            ->end() // config
+
+            /**
+             * Entry file
+             */
+            ->arrayNode('entry_file')
+                ->addDefaultsIfNotSet()
+				->children()
+
+					->booleanNode('enabled')
+						->defaultTrue()
+					->end()
+
+                    ->arrayNode('disabled_extensions')
+						->defaultValue([
+							'js',
+							'jsx',
+							'ts',
+							'tsx',
+							'coffee',
+							'es6',
+							'ls',
+						])
+						->prototype('scalar')
+						    ->info('For these extensions default webpack functionality will be used')
+                        ->end()
+					->end()
+
+					->arrayNode('enabled_extensions')
+						->defaultValue([])
+						->prototype('scalar')
+							->info(
+								'For these extensions file itself will be provided (not JS file). '
+								. 'Set to non-empty to override disabled extensions. Empty means all but disabled'
+							)
+                        ->end()
+					->end()
+
+					->arrayNode('type_map')
+						->defaultValue([
+							'css' => [
+								'less',
+								'scss',
+								'sass',
+								'styl',
+							],
+						])
+						->prototype('array')
+							->info(
+								'What output file type to use for what input file types. Used only for entry files. '
+								. 'Defaults to same file type - needed only when preprocessors are used'
+							)
+                        ->end()
+					->end()
+
+				->end()
+            ->end() // entry_file
+
+            /**
+             * Aliases
+             */
+            ->arrayNode('aliases')
+                ->addDefaultsIfNotSet()
+				->children()
+
+					->arrayNode('register_bundles')
+						->defaultValue($availableBundles)
+						->treatNullLike($availableBundles)
+						->prototype('scalar')
+							->validate()
+								->ifNotInArray($availableBundles)
+								->thenInvalid('%s is not a valid bundle.')
+							->end()
+						->end()
+					->end()
+
+					->scalarNode('path_in_bundle')
+						->defaultValue('Resources/assets')
+					->end()
+
+					->arrayNode('additional')
+						->treatNullLike([])
+						->useAttributeAsKey('name')
+						->prototype('scalar')->end()
+					->end()
+
+				->end()
+			->end() // aliases
+
+            /**
+             * Webpack and webpack-dev-server executables
+             */
+            ->arrayNode('bin')
+                ->addDefaultsIfNotSet()
+                ->children()
+
+                    ->booleanNode('disable_tty')
+                        ->defaultValue($this->environment !== 'dev')
+                    ->end()
+
+					/**
+					 * Webpack executable and arguments
+					 */
+                    ->arrayNode('webpack')
+                        ->addDefaultsIfNotSet()
+                        ->children()
+							->arrayNode('executable')
+								->defaultValue([
+									'node',
+									'node_modules/webpack/bin/webpack.js',
+								])
+								->prototype('scalar')->end()
+							->end()
+							->arrayNode('tty_prefix')
+								->defaultValue([])
+								->prototype('scalar')->end()
+							->end()
+							->arrayNode('arguments')
+								->defaultValue([])
+								->prototype('scalar')->end()
+							->end()
+                        ->end()
+                    ->end()
+
+					/**
+					 * Webpack dev server executable and arguments
+					 */
+                    ->arrayNode('dev_server')
+                        ->addDefaultsIfNotSet()
+                        ->children()
+                            ->arrayNode('executable')
+                                ->defaultValue([
+									'node',
+									'node_modules/webpack-dev-server/bin/webpack-dev-server.js',
+								])
+								->prototype('scalar')->end()
+                            ->end()
+							->arrayNode('tty_prefix')
+								->defaultValue([
+									'node',
+									'node_modules/webpack-dashboard/bin/webpack-dashboard.js',
+									'--',
+								])
+								->prototype('scalar')->end()
+                            ->end()
+                            ->arrayNode('arguments')
+                                ->defaultValue([
+									'--hot',
+									'--history-api-fallback',
+									'--inline',
+								])
+                                ->prototype('scalar')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+
+                ->end()
+            ->end() // bin
         ;
-        $suppressErrorsNode = $twig->scalarNode('suppress_errors')->defaultValue(
-            $this->environment === 'dev' ? true : 'ignore_unknowns'
-        );
-        $suppressErrorsNode
-            ->validate()
-            ->ifNotInArray(array(true, false, 'ignore_unknowns'))
-            ->thenInvalid('suppress_errors must be either a boolean or "ignore_unknowns"')
-        ;
-
-        $config = $children->arrayNode('config')->addDefaultsIfNotSet()->children();
-        $config->scalarNode('path')->defaultValue('%kernel.root_dir%/config/webpack.config.js');
-        $config->arrayNode('parameters')->treatNullLike(array())->useAttributeAsKey('name')->prototype('variable');
-
-        $entryFile = $children->arrayNode('entry_file')->addDefaultsIfNotSet()->children();
-        $entryFile->booleanNode('enabled')->defaultTrue();
-        $entryFile
-            ->arrayNode('disabled_extensions')
-            ->defaultValue(array('js', 'jsx', 'ts', 'coffee', 'es6', 'ls'))
-            ->prototype('scalar')
-            ->info('For these extensions default webpack functionality will be used')
-        ;
-        $entryFile
-            ->arrayNode('enabled_extensions')
-            ->defaultValue(array())
-            ->prototype('scalar')
-            ->info(
-                'For these extensions file itself will be provided (not JS file). '
-                . 'Set to non-empty to override disabled extensions. Empty means all but disabled'
-            )
-        ;
-        $entryFile
-            ->arrayNode('type_map')
-            ->defaultValue(array(
-                'css' => array('less', 'scss', 'sass', 'styl'),
-            ))
-            ->prototype('array')
-            ->info(
-                'What output file type to use for what input file types. Used only for entry files. '
-                . 'Defaults to same file type - needed only when preprocessors are used'
-            )
-        ;
-
-        $aliases = $children->arrayNode('aliases')->addDefaultsIfNotSet()->children();
-        $registerBundles = $aliases->arrayNode('register_bundles');
-        $registerBundles
-            ->defaultValue($availableBundles)
-            ->treatNullLike($availableBundles)
-        ;
-        $registerBundles
-            ->prototype('scalar')
-            ->validate()
-            ->ifNotInArray($availableBundles)
-            ->thenInvalid('%s is not a valid bundle.')
-        ;
-        $aliases->scalarNode('path_in_bundle')->defaultValue('Resources/assets');
-        $aliases->arrayNode('additional')->treatNullLike(array())->useAttributeAsKey('name')->prototype('scalar');
-
-        $bin = $children->arrayNode('bin')->addDefaultsIfNotSet()->children();
-
-        $bin->booleanNode('disable_tty')->defaultValue($this->environment !== 'dev');
-
-        $webpack = $bin->arrayNode('webpack')->addDefaultsIfNotSet()->children();
-        $webpack
-            ->arrayNode('executable')
-            ->defaultValue(array('node', 'node_modules/webpack/bin/webpack.js'))
-            ->prototype('scalar')
-        ;
-        $webpack->arrayNode('tty_prefix')->defaultValue(array())->prototype('scalar');
-        $webpack->arrayNode('arguments')->defaultValue(array())->prototype('scalar');
-
-        $devServer = $bin->arrayNode('dev_server')->addDefaultsIfNotSet()->children();
-        $devServer
-            ->arrayNode('executable')
-            ->defaultValue(array('node', 'node_modules/webpack-dev-server/bin/webpack-dev-server.js'))
-            ->prototype('scalar')
-        ;
-        $devServer->arrayNode('tty_prefix')
-            ->defaultValue(array('node', 'node_modules/webpack-dashboard/bin/webpack-dashboard.js', '--'))
-            ->prototype('scalar')
-        ;
-        $devServer->arrayNode('arguments')->defaultValue(array(
-            '--hot',
-            '--history-api-fallback',
-            '--inline',
-        ))->prototype('scalar');
 
         return $treeBuilder;
     }
 }
+
