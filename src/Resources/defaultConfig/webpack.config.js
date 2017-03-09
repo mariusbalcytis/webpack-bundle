c39f41979f3189ce3ad14b13231c01c955814a53
Marius Balčytis <marius.balcytis@gmail.com>
2017-03-10 01:54:43 +0200

    Clearer webpack configuration file with improved descriptions

--- src/Resources/defaultConfig/webpack.config.js
+++ src/Resources/defaultConfig/webpack.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// Modules
 var webpack = require('webpack');
+var path = require('path');
 var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var AssetsPlugin = require('assets-webpack-plugin');
@@ -10,67 +10,60 @@ var DashboardPlugin = require('webpack-dashboard/plugin');
 
 module.exports = function makeWebpackConfig(options) {
     /**
-     * Environment type
-     * BUILD is for generating minified builds
+     * Whether we are generating minified assets for production
      */
     var BUILD = options.environment === 'prod';
+
     /**
      * Whether we are running in dev-server mode (versus simple compile)
      */
     var DEV_SERVER = process.env.WEBPACK_MODE === 'watch';
 
     /**
-     * Config
-     * Reference: http://webpack.github.io/docs/configuration.html
-     * This is the object where all configuration gets set
+     * Whether we are running inside webpack-dashboard
      */
-    var config = {};
-
-    config.entry = options.entry;
-
-    config.resolve = {
-        alias: options.alias,
-        extensions: ['', '.js', '.jsx'],
-        modulesDirectories: ['node_modules', '']
-    };
+    var DASHBOARD = process.env.WEBPACK_DASHBOARD === 'enabled';
 
     var publicPath;
     if (options.parameters.dev_server_public_path && DEV_SERVER) {
         publicPath = options.parameters.dev_server_public_path;
 
-        // this is for both modes to maintain backwards compatibility
     } else if (options.parameters.public_path) {
         publicPath = options.parameters.public_path;
     } else {
         publicPath = DEV_SERVER ? 'http://localhost:8080/compiled/' : '/compiled/';
     }
 
-    config.output = {
-        // Absolute output directory
-        path: options.parameters.path ? options.parameters.path : __dirname + '/../../web/compiled/',
+    /**
+     * Config
+     * Reference: http://webpack.github.io/docs/configuration.html
+     * This is the object where all configuration gets set
+     */
+    var config = {
+        entry: options.entry,
+        resolve: {
+            alias: options.alias,
+            extensions: ['', '.js', '.jsx'],
+            modulesDirectories: ['node_modules', '']
+        },
 
-        // Output path from the view of the page
-        publicPath: publicPath,
+        output: {
+            // Absolute output directory
+            path: options.parameters.path ? options.parameters.path : __dirname + '/../../web/compiled/',
 
-        // Filename for entry points
-        // Only adds hash in build mode
-        filename: BUILD ? '[name].[chunkhash].js' : '[name].bundle.js',
+            // Output path from the view of the page
+            publicPath: publicPath,
 
-        // Filename for non-entry points
-        // Only adds hash in build mode
-        chunkFilename: BUILD ? '[name].[chunkhash].js' : '[name].bundle.js'
+            // Filename for entry points
+            // Only adds hash in build mode
+            filename: BUILD ? '[name].[chunkhash].js' : '[name].bundle.js',
+
+            // Filename for non-entry points
+            // Only adds hash in build mode
+            chunkFilename: BUILD ? '[name].[chunkhash].js' : '[name].bundle.js'
+        }
     };
 
-    /**
-     * Devtool
-     * Reference: http://webpack.github.io/docs/configuration.html#devtool
-     * Type of sourcemap to use per build type
-     */
-    if (BUILD) {
-        config.devtool = 'source-map';
-    } else {
-        config.devtool = 'eval';
-    }
 
     /**
      * Loaders
@@ -78,80 +71,110 @@ module.exports = function makeWebpackConfig(options) {
      * List: http://webpack.github.io/docs/list-of-loaders.html
      * This handles most of the magic responsible for converting modules
      */
-
-    // Initialize module
     config.module = {
-        preLoaders: [{
-            // query string is needed for URLs inside css files, like bootstrap
-            test: /\.(gif|png|jpe?g|svg)(\?.*)?$/i,
-            loader: 'image-webpack'
-        }],
-        loaders: [{
-            // JS LOADER
-            // Reference: https://github.com/babel/babel-loader
-            // Transpile .js files using babel-loader
-            // Compiles ES6 and ES7 into ES5 code
-            test: /\.jsx?$/i,
-            loaders: ['babel'],
-            exclude: /node_modules/
-        }, {
-            // ASSET LOADER
-            // Reference: https://github.com/webpack/file-loader
-            // Copy png, jpg, jpeg, gif, svg, woff, woff2, ttf, eot files to output
-            // Rename the file using the asset hash
-            // Pass along the updated reference to your code
-            // You can add here any file extension you want to get copied to your output
+        preLoaders: [
+            /**
+             * Minify PNG, JPEG, GIF and SVG images with imagemin
+             * Reference: https://github.com/tcoopman/image-webpack-loader
+             *
+             * See `config.imageWebpackLoader` for configuration options
+             *
+             * Query string is needed for URLs inside css files, like bootstrap
+             */
+            {
+                test: /\.(gif|png|jpe?g|svg)(\?.*)?$/i,
+                loader: 'image-webpack'
+            }
+        ],
 
-            // query string is needed for URLs inside css files, like bootstrap
-            test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*)?$/i,
+        loaders: [
+            /**
+             * Compiles ES6 and ES7 into ES5 code
+             * Reference: https://github.com/babel/babel-loader
+             */
+            {
+                test: /\.jsx?$/i,
+                loaders: ['babel'],
+                exclude: /node_modules/
+            },
 
-            // put original name in the destination filename, too
-            loader: 'file?name=[name].[hash].[ext]'
-        }, {
-            // HTML LOADER
-            // Reference: https://github.com/webpack/raw-loader
-            // Allow loading html through js
-            test: /\.html$/i,
-            loader: 'raw'
-        }]
+            /**
+             * Copy files to output directory
+             * Rename the file using the asset hash
+             * Pass along the updated reference to your code
+             *
+             * Reference: https://github.com/webpack/file-loader
+             *
+             * Query string is needed for URLs inside css files, like bootstrap
+             * Overwrites name parameter to put original name in the destination filename, too
+             */
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*)?$/i,
+                loader: 'file?name=[name].[hash].[ext]'
+            },
+
+            /**
+             * Loads HTML files as strings inside JavaScript - can be used for templates
+             *
+             * Reference: https://github.com/webpack/raw-loader
+             */
+            {
+                test: /\.html$/i,
+                loader: 'raw'
+            },
+
+            /**
+             * Allow loading CSS through JS
+             * Reference: https://github.com/webpack/css-loader
+             *
+             * postcss: Postprocess your CSS with PostCSS plugins
+             * Reference: https://github.com/postcss/postcss-loader
+             * See config.postcss for more information
+             *
+             * ExtractTextPlugin: Extract CSS files into separate ones to load directly
+             * Reference: https://github.com/webpack/extract-text-webpack-plugin
+             *
+             * If ExtractTextPlugin is disabled, use style loader
+             * Reference: https://github.com/webpack/style-loader
+             */
+            {
+                test: /\.css$/i,
+                loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss')
+            },
+
+            /**
+             * Compile LESS to CSS, then use same rules
+             * Reference: https://github.com/webpack-contrib/less-loader
+             */
+            {
+                test: /\.less$/i,
+                loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss!less?sourceMap')
+            },
+
+            /**
+             * Compile SASS to CSS, then use same rules
+             * Reference: https://github.com/webpack-contrib/sass-loader
+             */
+            {
+                test: /\.scss$/i,
+                loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss!sass?sourceMap')
+            }
+        ]
     };
-
-    // CSS LOADER
-    // Reference: https://github.com/webpack/css-loader
-    // Allow loading css through js
-    //
-    // Reference: https://github.com/postcss/postcss-loader
-    // Postprocess your css with PostCSS plugins
-    var cssLoader = {
-        test: /\.css$/i,
-        // Reference: https://github.com/webpack/extract-text-webpack-plugin
-        // Extract css files in production builds
-        //
-        // Reference: https://github.com/webpack/style-loader
-        // Use style-loader in development for hot-loading
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss')
-    };
-
-    // Add cssLoader to the loader list
-    config.module.loaders.push(cssLoader);
-
-    // add less support
-    config.module.loaders.push({
-        test: /\.less$/i,
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss!less?sourceMap')
-    });
-
-    // add sass support
-    config.module.loaders.push({
-        test: /\.scss$/i,
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss!sass?sourceMap')
-    });
-
 
     /**
-     * PostCSS
-     * Reference: https://github.com/postcss/autoprefixer-core
-     * Add vendor prefixes to your css
+     * Configuration for image-loader
+     * Reference: https://github.com/tcoopman/image-webpack-loader
+     */
+    config.imageWebpackLoader = options.parameters.image_loader_options || {
+        progressive: true,
+        optimizationLevel: 7
+    };
+
+    /**
+     * Add vendor prefixes to CSS
+     *
+     * Reference: https://github.com/postcss/autoprefixer
      */
     config.postcss = [
         autoprefixer({
@@ -165,47 +188,81 @@ module.exports = function makeWebpackConfig(options) {
      * List: http://webpack.github.io/docs/list-of-plugins.html
      */
     config.plugins = [
+        /**
+         * Used for CSS files to extract from JavaScript
+         * Reference: https://github.com/webpack/extract-text-webpack-plugin
+         */
         new ExtractTextPlugin(
             BUILD ? '[name].[hash].css' : '[name].bundle.css',
             {
                 disable: options.parameters.extract_css === false
             }
-        )
+        ),
+
+        /**
+         * Webpack plugin that emits a json file with assets paths - used by the bundle
+         * Reference: https://github.com/kossnocorp/assets-webpack-plugin
+         */
+        new AssetsPlugin({
+            filename: path.basename(options.manifest_path),
+            path: path.dirname(options.manifest_path)
+        }),
+
+        /**
+         * Adds assets loaded with extract-file-loader as chunk files to be available in generated manifest
+         * Used by the bundle to use binary files (like images) as entry-points
+         * Reference: https://github.com/mariusbalcytis/extract-file-loader
+         */
+        new ExtractFilePlugin()
     ];
 
-    var manifestPathParts = options.manifest_path.split('/');
-    config.plugins.push(new AssetsPlugin({filename: manifestPathParts.pop(), path: manifestPathParts.join('/')}));
-
-    // needed to use binary files (like images) as entry-points
-    // puts file-loader emitted files into manifest
-    config.plugins.push(new ExtractFilePlugin());
-
-    config.imageWebpackLoader = options.parameters.image_loader_options || {
-            progressive: true,
-            optimizationLevel: 7
-        };
-
-    if (process.env.WEBPACK_DASHBOARD === 'enabled') {
+    /**
+     * Adds CLI dashboard when compiling assets instead of the standard output
+     * Reference: https://github.com/FormidableLabs/webpack-dashboard
+     */
+    if (DASHBOARD) {
         config.plugins.push(new DashboardPlugin());
     }
 
-    // Add build specific plugins
+    /**
+     * Build specific plugins - used only in production environment
+     */
     if (BUILD) {
         config.plugins.push(
-            // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
-            // Only emit files when there are no errors
+            /**
+             * Only emit files when there are no errors
+             * Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+             */
             new webpack.NoErrorsPlugin(),
 
-            // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
-            // Dedupe modules in the output
+            /**
+             * Dedupe modules in the output
+             * Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
+             */
             new webpack.optimize.DedupePlugin(),
 
-            // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
-            // Minify all javascript, switch loaders to minimizing mode
+            /**
+             * Minify all javascript, switch loaders to minimizing mode
+             * Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+             */
             new webpack.optimize.UglifyJsPlugin(),
 
+            /**
+             * Assign the module and chunk ids by occurrence count
+             * Reference: https://webpack.github.io/docs/list-of-plugins.html#occurrenceorderplugin
+             */
             new webpack.optimize.OccurenceOrderPlugin(true)
         );
+    }
+
+    /**
+     * Devtool - type of sourcemap to use per build type
+     * Reference: http://webpack.github.io/docs/configuration.html#devtool
+     */
+    if (BUILD) {
+        config.devtool = 'source-map';
+    } else {
+        config.devtool = 'eval';
     }
 
     return config;
